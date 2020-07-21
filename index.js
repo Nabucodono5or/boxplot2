9ff0365fdd257a89e6e2c47b434fc836b2cc752a
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-07-21 11:11:11 -0300

    projeto de drogas relatadas em boxplot: iniciado

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import { select, selectAll } from "d3-selection";
+import { csv } from "d3-fetch";
 
-select("body").append("h1").html("Olá mundo");
+csv(require("./data/Accidental_Drug_Related_Deaths_2012-2018.csv")).then(
+  (data) => {
+    console.log(data);
+  }
+);
