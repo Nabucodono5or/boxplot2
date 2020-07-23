5c73c8d9bbf6696d088c78b0f05f5d3036c2d9e2
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-07-23 10:12:24 -0300

    adcionada contabilizações em algumas propriedades

--- index.js
+++ index.js
@@ -4,10 +4,31 @@ import { max, min } from "d3-array";
 
 csv(require("./data/Accidental_Drug_Related_Deaths_2012-2018.csv")).then(
   (data) => {
-    console.log(data);
+    let novo = formatandoDados(data);
     console.log(formatandoDados(data));
+    console.log(contabilizandoDados(novo));
   }
 );
+
+function contabilizandoDados(novo) {
+  novo.forEach((element) => {
+    let maiorIdade = max(element.idades, (d) => {
+      return d;
+    });
+    let menorIdade = min(element.idades, (d) => {
+      return d;
+    });
+    let mediaIdades = Math.round(element["somaIdades"] / element["number"]);
+    let mediana = (maiorIdade + menorIdade) / 2;
+
+    element["min"] = menorIdade;
+    element["max"] = maiorIdade;
+    element["media"] = mediaIdades;
+    element["mediana"] = Math.round(mediana);
+  });
+
+  return novo;
+}
 
 function formatandoDados(data) {
   let novo = [];
